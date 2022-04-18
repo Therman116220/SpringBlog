498a623f99118a86f83adffdb2991c1efdfc5268
Therman116220 <thermanhaynes@gmail.com>
2022-04-18 12:26:27 -0500

    first commit

---
+++ src/main/java/com/codeup/springblog/SpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogApplication.class, args);
+    }
+
+}
