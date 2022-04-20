14a39cde45cd3f8bef374160996d10fd43afff63
Therman116220 <thermanhaynes@gmail.com>
2022-04-20 12:00:07 -0500

    updated file

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,9 +1,9 @@
 package com.codeup.springblog;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,18 +11,29 @@ public class PostController {
 
     @GetMapping("/posts")
     @ResponseBody
-            public String postView(@PathVariable )
-}
+    public String postsIndex(){
+
+        return "Index for posts page :D";
+    }
 
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String postId(@PathVariable ) {
-return "e";
+    public String postView(@PathVariable long id){
+
+        return "Future page to show off id: " + id;
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String postCreate() {
-    return "create a new post";
-}
+    public String postCreate(){
+
+        return "Form for creating a post - pardon our dust!";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String submitPost(){
+        return "create a new post";
+    }
+
 }
